2f939370cf898f97d36968be4d2af5c074c3609f
isabelleduif <55750165+isabelleduif@users.noreply.github.com>
2022-04-25 16:32:48 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,35 @@
 // JavaScript Document
-console.log("Howdy!");
+
+var scherm = document.querySelector(".scherm ol");
+var buttonUp = document.querySelector(".up");
+var buttonDown = document.querySelector(".down");
+
+buttonUp.addEventListener("click", scrollUp);
+buttonDown.addEventListener("click", scrollDown);
+
+var scrollElement = 1;
+
+var jaartallen = ["jaartal1", "jaartal2", "jaartal3"];
+var jaartalFoto = document.querySelector(".jaartal1");
+
+function scrollUp() {
+    if (scrollElement == 1) {
+        scrollElement = 1;
+    } else {
+        scrollElement -= 1;
+    }
+
+    document.querySelector(".scherm ol li:nth-of-type(" + scrollElement + ")").scrollIntoView();
+    jaartalFoto.src = "../images/" + jaartallen[scrollElement - 1] + ".png";
+}
+
+function scrollDown() {
+    if (scrollElement == 3) {
+        scrollElement = 3;
+    } else {
+        scrollElement += 1;
+    }
+
+    document.querySelector(".scherm ol li:nth-of-type(" + scrollElement + ")").scrollIntoView();
+    jaartalFoto.src = "../images/" + jaartallen[scrollElement - 1] + ".png";
+}
